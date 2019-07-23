f58f5a96cdb5e26b461c7dcde438bebe586a4894
David Rozas <david.rozas@ibm.com>
2019-07-23 16:42:23 -0400

    Ingresado el menu de ataque en el main y su prog

--- pokemon-lib/src/com/ibm/pkmn/StartPkmn.java
+++ pokemon-lib/src/com/ibm/pkmn/StartPkmn.java
@@ -8,6 +8,8 @@ public class StartPkmn {
 		
 		Scanner sc= new Scanner(System.in);
 		int pok;
+		int pelea;
+		int atac;
 		System.out.println("M E N U");
 		System.out.println("---------------");
 		System.out.println("1.Charmander");
@@ -25,7 +27,30 @@ public class StartPkmn {
 			System.out.println("Nombre: "+ ((Charmander)pkn).getNombre()+"; energia: "+
 			((Charmander)pkn).getEnergia()+ "; Tipo: "+ ((Charmander)pkn).getTipo()+ "; Habilidad: "+
 			((Charmander)pkn).getHabilidad());
-
+			
+			System.out.println("----------------------------------------------");
+			System.out.println("inicio de batalla");
+			System.out.println("Elija su oponente");
+			System.out.println("2.Squartler");
+			System.out.println("3.Carterpy");
+			pelea=sc.nextInt();
+			if (pelea==2) {
+				System.out.println("Has escogido pelear contra Squartle");
+				System.out.println("1.ataque");
+				atac=sc.nextInt();
+				if(atac==1) {
+					int energy=100;
+					int resultado;
+					resultado = energy-50; 
+					System.out.println("Energia de Squartle: "+resultado);
+										
+					//System.out.println("Energia: "+);
+				
+				}
+				
+			}
+						
+			
 		} else if (pok==2) {
 			
 			//System.out.println("opcion 2");
